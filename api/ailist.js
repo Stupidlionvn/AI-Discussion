58df4953a3ee376d76d889d4c80821f6b657d1a5
QuestBig <31820678+Stupidlionvn@users.noreply.github.com>
2025-07-18 16:36:00 +0700

    Update ailist.js

--- api/ailist.js
+++ api/ailist.js
@@ -1,1 +1,6 @@
-
+// /api/ailist.js
+import fs from 'fs';
+export default function handler(req, res) {
+  const data = JSON.parse(fs.readFileSync('registrations.json', 'utf8'));
+  res.status(200).json(data);
+}
